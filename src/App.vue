a2e61850e24bc052877c280576d9f0419defefca
GuzmanAriel <guzmanariel525@gmail.com>
2022-10-14 10:43:55 -0500

    removed the field array

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,11 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <WebForm/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script setup>
+import WebForm from './components/WebForm.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
---
+++ src/components/WebForm.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Ariel Guzman's Endeavor Test <Icon icon="mdi-light:home" /></h1>
+  </div>
+</template>
+
+<script setup>
+import { Icon } from '@iconify/vue'
+
+</script>
